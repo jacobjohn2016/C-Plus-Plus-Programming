15393e1e289e605a3e35e146862a865b2ccf162b
Jacob <kapsjacob@gmail.com>
2019-03-28 21:23:43 +0530

    practice

--- Practice/copy_constructor.cpp
+++ Practice/copy_constructor.cpp
@@ -25,32 +25,8 @@ class Line{
         int *ptr;
 };
 
-//Member functions definitions including constructor
 Line::Line(int len){
-    cout << "Normal constructor allocating ptr" << endl;
 
-    //allocate memory for pointer
-    ptr = new int;
-    *ptr = len;
-}
-
-Line::Line(const Line &obj){
-    cout << "Copy constructor being created" << endl;
-    ptr = new int;
-    *ptr = *obj.ptr; //copy the value
-}
-
-Line::~Line(void){
-    cout << "Freeing Memory!" << endl;
-    delete ptr;
-}
-
-int Line::getLength(void){
-    return *ptr;
-}
-
-void display(Line obj){
-    cout << "Length of line: " << obj.getLength() << endl;
 }
 
 //main function
