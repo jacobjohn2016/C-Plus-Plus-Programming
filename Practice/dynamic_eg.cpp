84a50e3aeb22b9ebf2f0b35c6157479bd0a571c2
Jacob <kapsjacob@gmail.com>
2019-04-01 21:07:40 +0530

    dynamic mem allocate

--- Practice/dynamic_eg.cpp
+++ Practice/dynamic_eg.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
+class Numbers{
+    public:
+        int i,j;
+};
+
 int main(){
     int *arr;
     arr = new int[10];
     delete [] arr;
+
+    Numbers *num;
+    num = new Numbers;
+    (*num).i = 10;
+    cout << (*num).i << endl;
 }
