439a6f1c8be0cf7f566bb88f13fa01fde269eb28
Jacob <kapsjacob@gmail.com>
2019-04-01 22:41:27 +0530

    function overloading

--- Practice/function_overloading.cpp
+++ Practice/function_overloading.cpp
@@ -7,6 +7,22 @@ class printData{
             cout << "Printing int: " << i << endl;
         }
         void print(double f){
-            cout << "Printing double: " << i << endl;
+            cout << "Printing double: " << f << endl;
         }
+}
+
+int main(void)
+{
+    printData pd;
+
+    // call print to print integer
+    pd.print(5);
+
+    // call print to print double
+    pd.print(100.23);
+
+    // call print to print character
+    //pd.print("Hello C++");
+
+    return 0;
 }
